740fe12c04c2df5bd504d7540cabc52a8e83199e
Cedric Pelvet <cedric.pelvet@gmail.com>
2020-10-23 10:24:14 +0200

    Change region to eu-west-1

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ python -m venv .env
 # download requirements
 .env/bin/python -m pip install -r requirements.txt
 # create the key pair
-aws ec2 create-key-pair --key-name elk-key-pair --query 'KeyMaterial' --output text > elk-key-pair.pem --region us-east-1
+aws ec2 create-key-pair --key-name elk-key-pair --query 'KeyMaterial' --output text > elk-key-pair.pem --region us-west-1
 # update key_pair permissions
 chmod 400 elk-key-pair.pem
 # move key_pair to .ssh
